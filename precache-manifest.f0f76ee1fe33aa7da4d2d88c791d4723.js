9d8ca341bc45be9870a699453f29cf4100affdc8
Alexey Pyzhianov <pyzhyanovai@sibur.ru>
2019-09-27 21:59:08 +0300

    Updates

--- precache-manifest.f0f76ee1fe33aa7da4d2d88c791d4723.js
+++ precache-manifest.f0f76ee1fe33aa7da4d2d88c791d4723.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "625c1cb02f43f9d32fedef9db57224e6",
+    "revision": "8b3b669ebaac4f12bc8a9dc1c0517a86",
     "url": "/foreveralonedating-explorer/index.html"
   },
   {
-    "revision": "be7cb71a17cd0f33d7af",
-    "url": "/foreveralonedating-explorer/static/css/main.e024b7f3.chunk.css"
+    "revision": "e09207ed5dc0117f6c8b",
+    "url": "/foreveralonedating-explorer/static/css/main.ff4f6539.chunk.css"
   },
   {
     "revision": "59e9a066a3eb17048060",
     "url": "/foreveralonedating-explorer/static/js/2.e79f8ae4.chunk.js"
   },
   {
-    "revision": "be7cb71a17cd0f33d7af",
+    "revision": "e09207ed5dc0117f6c8b",
     "url": "/foreveralonedating-explorer/static/js/main.64afc8e1.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/foreveralonedating-explorer/precache-manifest.71f7a66eeb452c956ebd12a79334cfc0.js"
+  "/foreveralonedating-explorer/precache-manifest.f0f76ee1fe33aa7da4d2d88c791d4723.js"
 );
 
 self.addEventListener('message', (event) => {
